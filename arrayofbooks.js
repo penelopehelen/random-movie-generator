c8c942b6fae78a0fc1cf92fa702cd3056065a8cc
Penelope Helen <penelopehelen@live.co.uk>
2019-02-21 16:23:24 +0000

    Update arrayofbooks.js

--- arrayofbooks.js
+++ arrayofbooks.js
@@ -6,6 +6,6 @@ fetch("books.json").then(function(response) {
   var books = JSON.parse(books);
     document.getElementById("book_selector").addEventListener("click", function( event ) {
       var book = books[Math.floor(Math.random() *books.length)];
-      document.getElementById("book").innerHTML = book.title + + book.author;
+      document.getElementById("book").innerHTML = book.title; + " -- " + book.author;
   }, false);
 })
